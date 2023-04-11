dbb4081fd16e7c54ed29bc1608068ba2ea9e84f1
vladncode <vladncode@gmail.com>
2023-04-11 19:33:35 +0300

    Added stack navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,31 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { Home } from './src/screens/Home';
+import { Text, View } from 'react-native';
 
-function App(): JSX.Element {
-  return <Home />;
+const Stack = createStackNavigator();
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
